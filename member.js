3746331df6b804d873f3f0931bdad30a78efa82e
Sameer Singh <sameersingh2799@gmail.com>
2024-09-09 16:22:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    const member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 1000
+    }
+
+    return member;
+}
